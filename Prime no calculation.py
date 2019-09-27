ed1226ab9d8c5ffda1abf67a933bd57687261bc9
srialok01 <MS7@drive>
2019-09-27 23:55:49 +0530

    updated Next Prime no

--- Prime no calculation.py
+++ Prime no calculation.py
@@ -1,12 +1,32 @@
-# num1 = int(input("Enter first no 1 :"))
-# num2 = int(input("Enter second no 2 :"))
-#
-# for num in range (num1, num2):
+"""
+Program to print prime nos in range.
+"""
+
+# num1 = int(input("Enter number1: "))
+# num2 = int(input("Enter number2: "))
+# for num in range(num1, num2):
 #     if num>1:
-#         for i in range(2,num):
+#         for i in range(2, num):
 #             if num % i ==0:
 #                 break
 #         else:
 #             print(num)
+
+"""
+Program to print next prime no
+"""
+num = int(input("Enter number : "))
+for i in range(num+1, 2 * num):
+    if num > 1:
+        for j in range(2, i):
+
+            if i % j == 0:
+                break
+        else:
+
+            print(i)
+            break
+
+
 
 
