1e444dd4553a1950d79135a2841833a11fbc9396
srialok01 <MS7@drive>
2019-10-16 11:34:32 +0530

    Speaker Purchase and code refactoring

--- calculate_Palindrome.py
+++ calculate_Palindrome.py
@@ -2,12 +2,25 @@
 Calculate if given no is palindrome or not
 """
 
-def palindorme(num):
-    if num == num[::-1]:
-        print("palindrome")
-    else:
-        print("Not palindrome")
+
+# def palindorme(num):
+#     if num == num[::-1]:
+#         print("palindrome")
+#     else:
+#         print("Not palindrome")
+
+
+def palindrome(num):
+    count = 0
+    while count<3:
+        num = str(num)
+        if num == num[::-1]:
+            print("Next palindrome no is " + num)
+            count +=1
+
+        num = int(num)
+        num += 1
 
 
 number = input("Enter the number:")
-palindorme(number)
+palindrome(number)
